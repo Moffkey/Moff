3dd937b3d3559b5ca4c7628c067d7a8100b41a9c
sirotama <magicsirotama@gmail.com>
2016-09-03 00:49:26 +0900

    feature/event参照

--- application/core/my_controller.php
+++ application/core/my_controller.php
@@ -1,17 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class MY_Controller extends CI_Controller {
-
     function __construct() {
         parent::__construct();
-
         $this->load->library('session');
         $this->load->helper('url');
-
-        if( $this->session->userdata('auth_status') !== "TRUE" ) {
+        if(!$this->session->userdata('auth_status')) {
             $this->session->set_userdata('access_uri', $this->uri->uri_string());
-            redirect('login');
         }
     }
 }
