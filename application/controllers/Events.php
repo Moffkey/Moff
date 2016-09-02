3c6e4bf2af19d71a2e790790d8e78fbe591805fc
sirotama <magicsirotama@gmail.com>
2016-09-03 00:53:26 +0900

    createとedit

--- application/controllers/Events.php
+++ application/controllers/Events.php
@@ -10,7 +10,15 @@ class Events extends MY_Controller {
     public function detail($id)
     {
         $data['val'] = $id;
-        $this->load->view('events/index.php', $data);
+        $this->load->view('events/detail.php', $data);
+    }
+    public function create()
+    {
+        $this->load->view('events/create.php');
+    }
+    public function edit()
+    {
+        $this->load->view('events/edit.php');
     }
 
 }
