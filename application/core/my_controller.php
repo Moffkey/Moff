64fab7f0e7f74e0276ffbb8dc5e5905da1c81805
sirotama <magicsirotama@gmail.com>
2016-09-03 22:16:13 +0900

    login_status実装

--- application/core/my_controller.php
+++ application/core/my_controller.php
@@ -3,10 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class MY_Controller extends CI_Controller {
     function __construct() {
         parent::__construct();
-        $this->load->library('session');
         $this->load->helper('url');
+    }
+
+    public function login_status()
+    {
+        $this->load->library('session');
         if(!$this->session->userdata('auth_status')) {
             $this->session->set_userdata('access_uri', $this->uri->uri_string());
+            return false;
+        } else {
+            return true;
         }
     }
 }
