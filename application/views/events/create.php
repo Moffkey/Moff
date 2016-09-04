5f2b58b989df0917cfefb95a02656c2cd58841de
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-04 10:26:48 +0900

    formをviewごとに閉じるように

--- application/views/events/create.php
+++ application/views/events/create.php
@@ -2,4 +2,6 @@
 <h1>イベントの作成</h1>
 <?php echo form_open('', 'enctype="multipart/form-data"'); ?>
 <?php require_once(__DIR__."/_form.php") ?>
+<button type="submit" class="btn btn-success">イベントを登録</button>
+</form>
 <?php require_once(__DIR__."/../footer.php") ?>
