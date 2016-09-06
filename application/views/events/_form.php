58a06452069c5a4ec10077660fb0e9243e4fb5f2
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-06 13:45:04 +0900

    候補日仮UI

--- application/views/events/_form.php
+++ application/views/events/_form.php
@@ -20,11 +20,11 @@ $(function(){
     <label for="inputDescription">説明</label>
     <div class="row">
         <div class="col-sm-6">
-            <textarea class="form-control col-xs-6" id="inputDescription" rows=8 name="description"></textarea>
+            <textarea class="form-control" id="inputDescription" rows=8 name="description"></textarea>
         </div>
         <div class="col-sm-6">
             <div class="panel panel-default" id="inputPreview">
-                <div class="panel-heading">Preview</div>
+                <div class="panel-heading" style="max-height:3em;">Preview</div>
                 <div class="panel-body" style="padding:0;height:100%;">
                     <iframe style="width:100%;height:100%;border:0;" srcdoc="Preview"></iframe>
                 </div>
@@ -34,7 +34,28 @@ $(function(){
 </div>
 <div class="form-group">
     <label for="inputCandidateDate">候補日</label>
-    TODO
+    <ul>
+        <li>2016-11-04 05:14 <input type="hidden" name="candidate_date[]" value="2016-11-04 05:14">
+    </ul>
+    <div class="col-sm-8"><input type="datetime-local" class="form-control" id="candidate_date_input"></div>
+    <button class="btn btn-primary col-xs-12 col-sm-4" id="candidate_date_add">Add</button>
+    <script>
+    $(function(){
+        $("#candidate_date_add").click(function(e){
+            e.preventDefault();
+            var $li= $("<li>")
+            var indate=$("#candidate_date_input").val();
+            if(!indate) alert("日時を入力してください")
+            indate = indate.replace("T"," ");
+            $li.append(indate);
+            var $input=$("<input>")
+            $input.attr("type","hidden");
+            $input.attr("name","candidate_date[]");
+            $input.val(indate);
+            $(this).parent().children("ul").append($li)
+        })
+    })
+    </script>
 </div>
 <div class="form-group">
     <label for="inputThumbnail">サムネイル(任意)</label>
